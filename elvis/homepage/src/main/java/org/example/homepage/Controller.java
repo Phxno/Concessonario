5452baeb93a3ea6ba1ce80190dd5dfc493ae5a83
Ellvviss <spiriens03@gmail.com>
2024-05-09 20:46:44 +0200

    5° Commit: Creati stand per le macchine al centro della pagina, bloccata la dimensione della pagina a quella prestabilita(1024 x 768), fixato hover sui bottoni menu e login, blur e disattivazione dello sfondo quando si clicca il tasto menu,creato effetto ingrandimento quando si passa sopra allo stand di una macchina, lasciato lo spazio domani per il login che farò come slider da destra verso sinistra. Tutte le immagini sono all'interno della cartella images

--- elvis/homepage/src/main/java/org/example/homepage/Controller.java
+++ elvis/homepage/src/main/java/org/example/homepage/Controller.java
@@ -10,11 +10,13 @@ import javafx.scene.control.Menu;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.util.Duration;
-
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.scene.effect.BoxBlur;
 
 public class Controller  {
+
+    @FXML
+    private AnchorPane car_home;
+
     @FXML
     private Button menu;
 
@@ -26,28 +28,33 @@ public class Controller  {
 
     public void initialize(){
         slider.setTranslateX(-200); //impostiamo la posizione iniziale dello slider a -200 cosi da renderla invisibile appena parte l'applicazione
-        menu_slider();
+        menuback.setVisible(false); //rendiamo invisibile il tasto menuback cosi che appena avviamo il codice non sia possibile cliccarlo
+        menu_slider();  //chiamiamo la funzione menu_slider
     }
 
 
 
 
     public void menu_slider(){
-        menu.setOnMouseClicked(event -> slideMenuTo(0, false, true)); //gestione degli eventi: quando clicchiamo il tasto menu spostiamo lo slider a 0 e rendiamo visibile il tasto menuback
-        menuback.setOnMouseClicked(event -> slideMenuTo(-200, true, false)); //gestione degli eventi: quando clicchiamo il tasto menuback spostiamo lo slider a -200 e rendiamo visibile il tasto menu
+        BoxBlur blur = new BoxBlur(6, 6, 3); // Crea un'istanza di BoxBlur
+        menu.setOnMouseClicked(event -> slideMenuTo(0, false, true,blur)); //gestione degli eventi: quando clicchiamo il tasto menu spostiamo lo slider a 0 e rendiamo visibile il tasto menuback
+        menuback.setOnMouseClicked(event -> slideMenuTo(-200, true, false,null)); //gestione degli eventi: quando clicchiamo il tasto menuback spostiamo lo slider a -200 e rendiamo visibile il tasto menu
     }
 
-    private void slideMenuTo(int x, boolean menuVisible, boolean menubackVisible) {
-        TranslateTransition slide = new TranslateTransition();
-        slide.setDuration(Duration.seconds(0.4));
-        slide.setNode(slider);
+    private void slideMenuTo(int x, boolean menuVisible, boolean menubackVisible, BoxBlur blurEffect){
+        TranslateTransition slide = new TranslateTransition(); //creiamo un'istanza di TranslateTransition
+        slide.setDuration(Duration.seconds(0.4)); //tempo di transizione
+        slide.setNode(slider); //impostiamo il nodo su cui effettuare la transizione
 
-        slide.setToX(x);
-        slide.play();
+        slide.setToX(x); //impostiamo la posizione finale dello slider
+        slide.play(); //avviamo la transizione
 
         slide.setOnFinished((ActionEvent e)-> {
+            car_home.setEffect(blurEffect); // Imposta l'effetto di sfocatura sulla pagina principale
+            car_home.setDisable(blurEffect != null); // Disabilita car_home se l'effetto di sfocatura è applicato
             menu.setVisible(menuVisible);
             menuback.setVisible(menubackVisible);
         });
     }
+
 }
--- elvis/homepage/src/main/java/org/example/homepage/Homepage.java
+++ elvis/homepage/src/main/java/org/example/homepage/Homepage.java
@@ -14,6 +14,15 @@ public class Homepage extends Application {
         Scene scene = new Scene(fxmlLoader.load(), 1024, 768);  //dimensione finestra 1024x768 pixel
         stage.setTitle("Concessionario");
         stage.setScene(scene);
+
+
+        //limitiamo la dimensione della nostra pagina a 1024x768 pixel "chiedere al prof prima"
+        stage.setMinWidth(1024);
+        stage.setMinHeight(768);
+        stage.setMaxWidth(1024);
+        stage.setMaxHeight(768);
+
+
         stage.show();
     }
 
