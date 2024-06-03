017874ddad382b6a056aa94050c011869b36e174
Phxno <matteo.bertaiola.2003@gmail.com>
2024-06-03 22:20:46 +0200

    basic ui

---
+++ berta/demo/src/main/java/com/example/demo/DipendenteController.java
@@ -0,0 +1,51 @@
+package com.example.demo;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.fxml.FXMLLoader;
+
+public class DipendenteController {
+
+    @FXML
+    private Button Cliente;
+
+    @FXML
+    private BorderPane MenPrev;
+
+    @FXML
+    private Button Ordini;
+
+    @FXML
+    private Button Preventivi;
+
+    @FXML
+    private Button Nay;
+
+    @FXML
+    private Button Okay;
+
+    @FXML
+    private Label UData;
+
+    public void initialize(){
+      MenPrev.setVisible(false);
+    }
+
+    private void spawnPrev(){
+      MenPrev.setVisible(true);
+    }
+    
+    @FXML
+    void Okay(ActionEvent event) throws IOException{
+      MenPrev.setVisible(false);
+    }
+
+    @FXML
+    void Prevs(ActionEvent event){
+      spawnPrev();
+    }
+
+}
