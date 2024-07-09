7b97c199fee7c1e6d4f21dfa256876ee3471dc7c
Phxno <matteo.bertaiola.2003@gmail.com>
2024-07-09 15:12:41 +0200

    Dipendente loggato temporaneamente e creazione file preventivi e ordini

--- Progetto/src/main/java/org/uni/progetto/homepage/DipendenteController.java
+++ Progetto/src/main/java/org/uni/progetto/homepage/DipendenteController.java
@@ -1,5 +1,4 @@
 package org.uni.progetto.homepage;
-import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
@@ -7,6 +6,8 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.*;
 import com.google.gson.*;
 import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 
 
 public class DipendenteController {
@@ -53,23 +54,23 @@ public class DipendenteController {
 
     private String getPreventivi(){
       String file = "dati_utente.json";
-      JsonObject dati = new JsonObject();
-            //proviamo a leggere il file dati_utente.json
-            JsonArray dati_presenti;
-            try (FileReader reader = new FileReader(file)) {
-                JsonElement parsed = JsonParser.parseReader(reader);
-                if (parsed.isJsonArray()) {
-                    dati_presenti = parsed.getAsJsonArray();
-                } else {
-                    dati_presenti = new JsonArray();
-                }
-                
-                //se il file esiste, leggiamo i dati, userData contiene i dati dell'utente sottoforma di oggetto JSON
-            } catch (IOException | JsonSyntaxException e) {
-                //se il file non esiste, creiamo un nuovo array vuoto
-                dati_presenti = new JsonArray();
-            }
-      return "";
+      Gson gson = new Gson();
+      String userData = "";
+
+      try (Reader reader = new FileReader(file)){
+        JsonArray usersArray = gson.fromJson(reader, JsonArray.class);
+            //Creiamo un oggetto JSON per ogni utente
+        for (JsonElement userElement : usersArray) {
+          JsonObject userObject = userElement.getAsJsonObject();
+          if (userObject.get("type-user").getAsInt() == 0){
+            userData = userObject.get("name").getAsString().concat(" ").concat(userObject.get("surname").getAsString());
+          }
+        }
+      } catch (IOException e){
+        e.printStackTrace();
+      }
+      return userData;
+
     }
 
 }
